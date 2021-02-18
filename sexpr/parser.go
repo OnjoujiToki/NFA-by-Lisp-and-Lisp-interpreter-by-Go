316b6034655717c174d18c81e8c7a91920066f90
gethshap <zhchuang@ucdavis.edu>
2021-02-17 20:11:33 -0800

    single number and symbol

--- sexpr/parser.go
+++ sexpr/parser.go
@@ -19,6 +19,43 @@ type Parser interface {
 	Parse(string) (*SExpr, error)
 }
 
+type myParserInstance struct{}
+
+func (myParserInstance) Parse(s string) (*SExpr, error) {
+	lexer := newLexer(s)
+
+	//read the first line
+	tok, err := lexer.next()
+
+	if err != nil {
+		return nil, ErrParser
+	}
+
+	//EOF
+	if tok.typ == tokenEOF {
+		return mkNil(), nil
+	}
+
+	//if the first NUMBER
+	if tok.typ == tokenNumber {
+		subtok, suberr := lexer.next()
+		if suberr != nil || subtok.typ != tokenEOF {
+			return nil, ErrParser
+		}
+		return mkNumber(tok.num), nil
+	}
+
+	//if the first Symbol
+	if tok.typ == tokenSymbol {
+		subtok, suberr := lexer.next()
+		if suberr != nil || subtok.typ != tokenEOF {
+			return nil, ErrParser
+		}
+		return mkSymbol(tok.literal), nil
+	}
+
+}
+
 func NewParser() Parser {
 	panic("TODO: implement NewParser")
 }
